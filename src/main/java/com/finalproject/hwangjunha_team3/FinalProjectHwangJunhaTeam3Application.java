04f0d1dd4554d2b7dfc59f59965191e1ea285389
lay423 <junpang486@gmail.com>
2022-12-20 10:25:02 +0900

    프로젝트 빌드

---
+++ src/main/java/com/finalproject/hwangjunha_team3/FinalProjectHwangJunhaTeam3Application.java
@@ -0,0 +1,13 @@
+package com.finalproject.hwangjunha_team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectHwangJunhaTeam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectHwangJunhaTeam3Application.class, args);
+    }
+
+}
