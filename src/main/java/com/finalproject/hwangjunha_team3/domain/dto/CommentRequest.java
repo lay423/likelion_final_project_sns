84e8f1a80f55d83c66e5b96f61b03ac60b375535
lay423 <junpang486@gmail.com>
2023-01-03 10:17:46 +0900

    [add] 댓글 등록 testCode 작성

---
+++ src/main/java/com/finalproject/hwangjunha_team3/domain/dto/CommentRequest.java
@@ -0,0 +1,14 @@
+package com.finalproject.hwangjunha_team3.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CommentRequest {
+    private String comment;
+}
---
+++ src/main/java/com/finalproject/hwangjunha_team3/domain/dto/CommentResponse.java
@@ -0,0 +1,14 @@
+package com.finalproject.hwangjunha_team3.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentResponse {
+    private Integer id;
+}
